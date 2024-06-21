6935732779431c035bfcab54be0d9a075ab2f8b7
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-06-21 19:01:21 +0100

    Fixed nft size

--- packages/nextjs/components/Card.tsx
+++ packages/nextjs/components/Card.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Popover from "./Popover";
 import { Token } from "~~/actions/m3ters";
 
@@ -6,11 +5,9 @@ function Card({ token }: { token: Token }) {
   return (
     <Popover token={token}>
       <div
-        className={`block mx-auto cursor-pointer relative space-y-2 bg-black/80 dark:bg-neutral-900/70 xl:h-[330px] xl:w-[300px] 2xl:w-[300px] 2xl:h-[330px] lg:w-[300px] lg:h-[330px] md:w-[300px] p-3 md:h-[330px] w-[300px] h-[330px] rounded-lg`}
+        className={`block mx-auto cursor-pointer relative space-y-2 bg-black/80 dark:bg-neutral-900/70 xl:h-[400px] xl:w-[300px] 2xl:w-[300px] 2xl:h-[400px] lg:w-[300px] lg:h-[400px] md:w-[300px] p-3 md:h-[400px] w-[300px] h-[400px] rounded-lg`}
       >
-        {token.image && (
-          <Image src={token.image} alt="nft" width={270} height={200} className={`w-[270px] h-[200px] block mx-auto`} />
-        )}
+        {token.image && <img src={token.image} alt="nft" width={270} className={`w-[270px]  block mx-auto`} />}
         <p className={`text-[18px] font-[600] dark:text-white text-white`}>{token.name}</p>
         <p className={`w-[97%] break-words italic text-[12px] font-thin text-slate-400 dark:text-neutral-400`}>
           {token.description}
