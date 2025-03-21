21cfcb6e7c83d9c8be2ef8a1f380377370aeb1e1
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-03-21 21:40:50 +0100

    chore: fixed setTariff args

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,6 @@
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import NotConnected from "~~/components/NotConnected";
-import Skeleton from "~~/components/Skeleton";
 import Tokenlist from "~~/components/Tokenlist";
 import useTokens from "~~/hooks/useTokens";
 
@@ -13,10 +12,7 @@ const Home: NextPage = () => {
   if (isConnected) {
     if (tokens && tokens.length > 0) {
       return <Tokenlist tokens={tokens} />;
-    } else
-      return (
-        <div>Loading ....</div>
-      );
+    } else return <div>Loading ....</div>;
   } else return <NotConnected />;
 };
 
